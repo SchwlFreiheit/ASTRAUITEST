2056977bb27cf68b1fa1455707f06e72ac647abf
SchwlFreiheit <44237419+SchwlFreiheit@users.noreply.github.com>
2026-08-28 22:54:27 +0900

    Apply spatial tuning pass

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './styles/layout.css'
 import './styles/components.css'
 import './styles/motion.css'
 import './styles/spatial.css'
+import './styles/spatial-tune.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
