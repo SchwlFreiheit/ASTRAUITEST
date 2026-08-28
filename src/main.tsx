949110465e512e3c294ab44f3061c32414c4cc73
SchwlFreiheit <44237419+SchwlFreiheit@users.noreply.github.com>
2026-08-28 23:06:10 +0900

    Enable secondary spatial workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './styles/spatial.css'
 import './styles/spatial-tune.css'
 import './styles/spatial-polish.css'
 import './styles/focus.css'
+import './styles/secondary-spatial.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
